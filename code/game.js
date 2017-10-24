89f54164d006a321a1f17cbd7af4b7f129868631
Lundy Lawrence <33065238+LundyLawrence@users.noreply.github.com>
2017-10-24 12:03:09 -0400

    Add files via upload

---
+++ code/game.js
@@ -0,0 +1,93 @@
+function Level(plan) {
+  // Use the length of a single row to set the width of the level
+  this.width = plan[0].length;
+  // Use the number of rows to set the height
+
+  this.height = plan.length;
+
+  // Store the individual tiles in our own, separate array
+  this.grid = [];
+
+  // Loop through each row in the plan, creating an array in our grid
+  for (var y = 0; y < this.height; y++) {
+    var line = plan[y], gridLine = [];
+
+    // Loop through each array element in the inner array for the type of the tile
+    for (var x = 0; x < this.width; x++) {
+      // Get the type from that character in the string. It can be 'x', '!' or ' '
+      // If the character is ' ', assign null.
+
+      var ch = line[x], fieldType = null;
+
+      // Use if and else to handle the two cases
+      if (ch == "x")
+        fieldType = "wall";
+      // Because there is a third case (space ' '), use an "else if" instead of "else"
+      else if (ch == "!")
+        fieldType = "lava";
+      else if (ch == "y")
+        fieldType = "floater";
+	
+      // "Push" the fieldType, which is a string, onto the gridLine array (at the end).
+      gridLine.push(fieldType);
+    }
+    // Push the entire row onto the array of rows.
+    this.grid.push(gridLine);
+  }
+}
+
+// Helper function to easily create an element of a type provided 
+// and assign it a class.
+function elt(name, className) {
+  var elt = document.createElement(name);
+  if (className) elt.className = className;
+  return elt;
+}
+
+// Main display class. We keep track of the scroll window using it.
+function DOMDisplay(parent, level) {
+
+// this.wrap corresponds to a div created with class of "game"
+  this.wrap = parent.appendChild(elt("div", "game"));
+  this.level = level;
+
+  // In this version, we only have a static background.
+  this.wrap.appendChild(this.drawBackground());
+}
+
+var scale = 20;
+
+DOMDisplay.prototype.drawBackground = function () {
+   var table = elt('table', 'background');
+   table.style.width = this.level.width * scale + 'px';
+
+   // Assign a class to new row element directly from the string
+   // from each tile in grid
+   for (var i=0; i<this.level.grid.length; i++)
+   {
+      var rowElt = table.appendChild(elt('tr'));
+      rowElt.style.height = scale + 'px';
+   
+      for (var j=0; j<this.level.grid[i].length; j++)
+      {
+         rowElt.appendChild(elt('td', this.level.grid[i][j]));
+       }
+    }
+
+    return table;
+}
+
+
+// Organize a single level and begin animation
+function runLevel(level, Display) {
+  var display = new Display(document.body, level);
+}
+
+function runGame(plans, Display) {
+  function startLevel(n) {
+    // Create a new level using the nth element of array plans
+    // Pass in a reference to Display function, DOMDisplay (in index.html).
+    runLevel(new Level(plans[n]), Display);
+  }
+  startLevel(0);
+}
